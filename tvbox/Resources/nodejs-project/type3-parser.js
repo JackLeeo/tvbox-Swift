30a4b8643d4673d3e191bd6c4450ec1c525f17b1
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-09 14:06:42 +0800

    Update type3-parser.js

--- tvbox/Resources/nodejs-project/type3-parser.js
+++ tvbox/Resources/nodejs-project/type3-parser.js
@@ -4,22 +4,20 @@ const https = require('https');
 const vm = require('vm');
 const URL = require('url');
 
-// 监听来自 Swift 的消息
 rn_bridge.channel.on('message', (msg) => {
     try {
         const type3Data = JSON.parse(msg);
         const requestId = type3Data.id;
-        
+
         if (!type3Data.url) {
             throw new Error('缺少源地址');
         }
-        
+
         fetchRemoteScript(type3Data.url, type3Data.headers, 10000)
             .then(remoteScript => {
                 const sandbox = {
                     result: {},
                     setResult: (data) => { sandbox.result = data; },
-                    log: (msg) => console.log(`[Type3] ${msg}`),
                     console: console,
                     require: require,
                     module: module,
@@ -30,7 +28,7 @@ rn_bridge.channel.on('message', (msg) => {
                     filename: 'type3-remote.js',
                     timeout: 10000
                 });
-                
+
                 rn_bridge.channel.send(JSON.stringify({
                     id: requestId,
                     success: true,
@@ -57,13 +55,13 @@ function fetchRemoteScript(url, headers, timeout) {
     return new Promise((resolve, reject) => {
         const parsedUrl = URL.parse(url);
         const client = parsedUrl.protocol === 'https:' ? https : http;
-        
+
         const req = client.get(url, { headers }, (res) => {
             let data = '';
             res.on('data', (chunk) => data += chunk);
             res.on('end', () => resolve(data));
         });
-        
+
         req.on('error', (err) => reject(new Error(`加载脚本失败: ${err.message}`)));
         req.setTimeout(timeout, () => {
             req.destroy();
